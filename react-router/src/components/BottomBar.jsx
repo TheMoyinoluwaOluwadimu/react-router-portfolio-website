e713b8efd1a6e741cd7812458ba317cd577f8ee9
unknowncoderboy <sammyphonist@gmail.com>
2022-12-21 17:12:15 +0100

    home page designed and pages linked

---
+++ react-router/src/components/BottomBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function BottomBar() {
+    return (
+        <footer>
+            {/* Fill in your actual details here */}
+            <ul>
+            <li>Name: Oluwadimu Samuel | </li>
+                <li>Matric Number: 21/0677 | </li>
+                <li>Department: Computer Science</li>
+            </ul>
+        </footer>
+    );
+}
+
+export default BottomBar;
---
+++ react-router/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../App.css";
+function Home() {
+    return <div>
+        <h1>Hello, i'm <em>Oluwadimu Samuel</em>, <br></br>a <em>UI/UX</em> Designer & a Frontend Developer</h1>
+        <p>I'm a product designer and a frontend developer based in Lagos, Nigeria<br></br> with experience in delivering end-to-end UX/UI design for software products. <br></br>I'm passionate about improving the lives of others through design<br></br> and am constantly looking to learn new things everyday.</p>
+        <img src="src\IMG_E1650.JPG" alt="my image"></img>
+    </div>;
+}
+
+export default Home;
